c485b251aa922646190dfdfc810cb03d10540916
Cyxapb-BLR <cyxapb-511@yandex.ru>
2021-06-15 14:39:05 -0400

    4_added logs and methods in MealRestController(getAll, create,upd,del)

--- src/main/java/ru/javawebinar/topjava/web/meal/MealRestController.java
+++ src/main/java/ru/javawebinar/topjava/web/meal/MealRestController.java
@@ -1,8 +1,48 @@
 package ru.javawebinar.topjava.web.meal;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.service.MealService;
+import ru.javawebinar.topjava.util.ValidationUtil;
+import ru.javawebinar.topjava.web.SecurityUtil;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static ru.javawebinar.topjava.util.ValidationUtil.assureIdConsistent;
+import static ru.javawebinar.topjava.util.ValidationUtil.checkNew;
 
 public class MealRestController {
-    private MealService service;
+    private final Logger log = LoggerFactory.getLogger(getClass());
+    private final MealService service;
 
+    public MealRestController(MealService service) {
+        this.service = service;
+    }
+
+    public List<Meal> getAll() {
+        log.info("getAll");
+        return new ArrayList<>(service.getAll(SecurityUtil.authUserId()));
+    }
+
+    public Meal create(Meal meal) {
+        int userId = SecurityUtil.authUserId();
+        log.info("create {}, userId = {}", meal, userId);
+        checkNew(meal);
+        return service.create(meal, userId);
+    }
+
+    public void delete(int id) {
+        int userId = SecurityUtil.authUserId();
+        log.info("delete mealId = {}, userId = {}", id, userId);
+        service.delete(id, userId);
+    }
+
+    public Meal update(Meal meal, int id) {
+        int userId = SecurityUtil.authUserId();
+        log.info("update {}, userId = {}", meal, userId);
+        assureIdConsistent(meal, userId);
+        return service.update(meal, userId);
+    }
 }
