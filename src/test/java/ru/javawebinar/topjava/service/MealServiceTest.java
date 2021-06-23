7004456e2e0f172f81d94b4f4850eac8c4848d4b
Cyxapb-BLR <cyxapb-511@yandex.ru>
2021-06-23 13:08:53 -0400

    created MealServiceTest, MealTestData, test meals, implemented created test, get test

---
+++ src/test/java/ru/javawebinar/topjava/service/MealServiceTest.java
@@ -0,0 +1,61 @@
+package ru.javawebinar.topjava.service;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.bridge.SLF4JBridgeHandler;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.jdbc.SqlConfig;
+import org.springframework.test.context.junit4.SpringRunner;
+import ru.javawebinar.topjava.model.Meal;
+
+import static ru.javawebinar.topjava.UserTestData.USER_ID;
+import static ru.javawebinar.topjava.web.MealTestData.*;
+
+@ContextConfiguration({
+        "classpath:spring/spring-app.xml",
+        "classpath:spring/spring-db.xml"
+})
+@RunWith(SpringRunner.class)
+@Sql(scripts = "classpath:db/populateDB.sql", config = @SqlConfig(encoding = "UTF-8"))
+public class MealServiceTest {
+    static {
+        SLF4JBridgeHandler.install();
+    }
+
+    @Autowired
+    private MealService service;
+
+    @Test
+    public void get() {
+        Meal meal = service.get(TESTED_MEAL_ID, USER_ID);
+        assertMatch(meal, userMeal1);
+    }
+
+    @Test
+    public void delete() {
+    }
+
+    @Test
+    public void getBetweenInclusive() {
+    }
+
+    @Test
+    public void getAll() {
+    }
+
+    @Test
+    public void update() {
+    }
+
+    @Test
+    public void create() {
+        Meal createdMeal = service.create(getNew(), USER_ID);
+        Integer id = createdMeal.getId();
+        Meal newMeal = getNew();
+        newMeal.setId(id);
+        assertMatch(createdMeal, newMeal);
+        assertMatch(service.get(id, USER_ID), newMeal);
+    }
+}
---
+++ src/test/java/ru/javawebinar/topjava/web/MealTestData.java
@@ -0,0 +1,65 @@
+package ru.javawebinar.topjava.web;
+
+import ru.javawebinar.topjava.model.Meal;
+
+import java.time.LocalDateTime;
+import java.time.Month;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static ru.javawebinar.topjava.model.AbstractBaseEntity.START_SEQ;
+
+public class MealTestData {
+
+    public final static int TESTED_MEAL_ID = START_SEQ + 2;
+
+    public static final int NOT_FOUND = 10;
+
+    public static final Meal userMeal1 = new Meal(TESTED_MEAL_ID, LocalDateTime.of(2020, Month.JANUARY, 30, 10, 0), "Завтрак", 500);
+    public static final Meal userMeal2 = new Meal(TESTED_MEAL_ID+1, LocalDateTime.of(2020, Month.JANUARY, 30, 13, 0), "Обед", 1000);
+    public static final Meal userMeal3 = new Meal(TESTED_MEAL_ID+2, LocalDateTime.of(2020, Month.JANUARY, 30, 20, 0), "Ужин", 500);
+    public static final Meal userMeal4 = new Meal(TESTED_MEAL_ID+3, LocalDateTime.of(2020, Month.JANUARY, 31, 0, 0), "Еда на граничное значение", 100);
+    public static final Meal userMeal5 = new Meal(TESTED_MEAL_ID+4, LocalDateTime.of(2020, Month.JANUARY, 31, 10, 0), "Завтрак", 1000);
+    public static final Meal userMeal6 = new Meal(TESTED_MEAL_ID+5, LocalDateTime.of(2020, Month.JANUARY, 31, 13, 0), "Обед", 500);
+    public static final Meal userMeal7 = new Meal(TESTED_MEAL_ID+6, LocalDateTime.of(2020, Month.JANUARY, 31, 20, 0), "Ужин", 410);
+
+    public static final Meal adminMeal1 = new Meal(TESTED_MEAL_ID+7, LocalDateTime.of(2021, Month.JUNE, 21, 10, 0), "Завтрак админа", 500);
+    public static final Meal adminMeal2 = new Meal(TESTED_MEAL_ID+8, LocalDateTime.of(2021, Month.JUNE, 21, 20, 0), "Ужин админа", 410);
+
+
+    public static final List<Meal> userMeals = Stream.of(userMeal1, userMeal2, userMeal3, userMeal4, userMeal5, userMeal6, userMeal7)
+            .sorted(Comparator.comparing(Meal::getDateTime).reversed())
+            .collect(Collectors.toList());
+
+    public static final List<Meal> adminMeals = Stream.of(adminMeal1, adminMeal2)
+            .sorted(Comparator.comparing(Meal::getDateTime).reversed())
+            .collect(Collectors.toList());
+
+    public static Meal getNew() {
+        return new Meal(LocalDateTime.now(), "New Test Meal", 600);
+    }
+
+    public static Meal getUpdateForUser() {
+        Meal meal = new Meal(userMeal1);
+        meal.setDateTime(LocalDateTime.now());
+        meal.setDescription("Updated meal");
+        meal.setCalories(300);
+        return meal;
+    }
+
+    public static void assertMatch(Meal actual, Meal expected) {
+        assertThat(actual).usingRecursiveComparison().isEqualTo(expected);
+    }
+
+    public static void assertMatch(Iterable<Meal> actual, Meal... expected) {
+        assertMatch(actual, Arrays.asList(expected));
+    }
+
+    public static void assertMatch(Iterable<Meal> actual, Iterable<Meal> expected) {
+        assertThat(actual).usingElementComparatorIgnoringFields("registered", "roles").isEqualTo(expected);
+    }
+}
